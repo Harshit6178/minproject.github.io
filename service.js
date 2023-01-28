6d6b6a18edc369aa67426bb77fa261e8ecf46f24
Harshit6178 <123722198+Harshit6178@users.noreply.github.com>
2023-01-28 05:48:20 +0530

    Update service.js

--- service.js
+++ service.js
@@ -1,28 +1,24 @@
-// Ensure that the browser supports the service worker API
+
+//Registration of Service Worker
 if (navigator.serviceWorker) {
-	// Start registration process on every page load
 	window.addEventListener('load', () => {
-		navigator.serviceWorker
-			// The register function takes as argument
-			// the file path to the worker's file
+	navigator.serviceWorker
 			.register('service_worker.js')
-			// Gives us registration object
 			.then(reg => console.log('Service Worker Registered'))
 			.catch(swErr => console.log(
 					`Service Worker Installation Error: ${swErr}}`));
 		});
 	}
 
-var cacheAll = false;
-var CACHE_NAME = 'webapk-cache';
+var call = false;
+var cname = 'webapk-cache';
 
 
 // Install Event
 self.addEventListener('install', function(event) {
 	console.log("[SW] install event: ",event);
-	// Perform install steps
 	event.waitUntil(
-		caches.open(CACHE_NAME).then(
+		caches.open(cname).then(
 			function(cache) {
 				console.log('[SW] Opened cache: ',cache);
 				return cache.addAll(urlsToCache);
@@ -39,13 +35,68 @@ self.addEventListener('fetch', function(event) {
 		caches.match(event.request).then(
 			function(response) {
 				if (response) return response;
-				else if (!cacheAll || urlsNotToCache.indexOf(event.request) !== -1) return fetch(event.request);
+				else if (!call || urlsNotToCache.indexOf(event.request) !== -1) return fetch(event.request);
 				else {
 					fetch(event.request).then(
 						function(response) {
 							if(!response || response.status !== 200 || response.type !== 'basic') return response;
 							var responseToCache = response.clone();
-							caches.open(CACHE_NAME).then(
+							caches.open(cname).then(
+								function(cache) {
+									cache.put(event.request, responseToCache);
+								}
+							);
+							return response;
+						}
+					);
+				}
+			}
+		)
+);
+});
+//Registration of Service Worker
+if (navigator.serviceWorker) {
+	window.addEventListener('load', () => {
+	navigator.serviceWorker
+			.register('service_worker.js')
+			.then(reg => console.log('Service Worker Registered'))
+			.catch(swErr => console.log(
+					`Service Worker Installation Error: ${swErr}}`));
+		});
+	}
+
+var call = false;
+var cname = 'webapk-cache';
+
+
+// Install Event
+self.addEventListener('install', function(event) {
+	console.log("[SW] install event: ",event);
+	event.waitUntil(
+		caches.open(cname).then(
+			function(cache) {
+				console.log('[SW] Opened cache: ',cache);
+				return cache.addAll(urlsToCache);
+			}
+		)
+	);
+});
+
+
+// Fetch Event
+self.addEventListener('fetch', function(event) {
+	console.log("[SW] fetch event: ",event);
+	event.respondWith(
+		caches.match(event.request).then(
+			function(response) {
+				if (response) return response;
+				else if (!call || urlsNotToCache.indexOf(event.request) !== -1) return fetch(event.request);
+				else {
+					fetch(event.request).then(
+						function(response) {
+							if(!response || response.status !== 200 || response.type !== 'basic') return response;
+							var responseToCache = response.clone();
+							caches.open(cname).then(
 								function(cache) {
 									cache.put(event.request, responseToCache);
 								}
